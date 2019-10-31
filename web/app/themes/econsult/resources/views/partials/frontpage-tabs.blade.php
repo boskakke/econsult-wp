eae023fb39f4c5b8bbb10b0c76a07c6673fa5b56
Bo Skakke <bo@skakke.dk>
2019-10-31 17:05:14 +0100

    removed menus

--- web/app/themes/econsult/resources/views/partials/frontpage-tabs.blade.php
+++ web/app/themes/econsult/resources/views/partials/frontpage-tabs.blade.php
@@ -5,21 +5,8 @@
 @if($template)
 
 <div class="tabs">
-	<div class="tabs__top">
-		
-		<ul class="main-tabs">
-			<li class="main-tabs__item <?php if($template == 'private'): ?> active <?php endif; ?>" data-tab="privat">
-				<a href="#" class="main-tabs__link">
-					Privat 
-				</a>
-			</li>
-			<li class="main-tabs__item <?php if($template == 'business'): ?> active <?php endif; ?>" data-tab="erhverv">
-				<a href="#" class="main-tabs__link ">
-					Erhverv
-				</a>
-			</li>
-		</ul>
-
+	
+	<div class="tabs__bottom">
 		@if(get_field('txt_phone','options'))
 		<div class="contact__desktop ">
 			<div class="p-2">
@@ -31,24 +18,6 @@
 		</div>
 		@endif	
 	</div>
-	<div class="tabs__bottom">
-		@php
-		$menu_private_class =     'tabs-menu';
-		$menu_business_class =    'tabs-menu';
-		if ($template ==  'private') {
-			$menu_private_class .= ' active';
-		} else {
-			$menu_business_class .= ' active';
-		}
-		@endphp
-		
-		{!! 
-			wp_nav_menu(['menu' => 'private_navigation', 'menu_class' => $menu_private_class , 'menu_id' => 'privat', 'container' => false, 'container_id' => 'privat']) !!}	
-			
-
-			{!! 
-				wp_nav_menu(['menu' => 'business_navigation', 'menu_class' => $menu_business_class, 'menu_id' => 'erhverv', 'container' => false, 'container_id' => 'business']) !!}	
-			</div>
 </div>
 
 @endif
