eb8cc7723e79266f88261e28b6effcf23048e306
Bo Skakke <bo@skakke.dk>
2019-11-02 17:20:28 +0100

    ga

--- web/app/themes/econsult/resources/views/partials/head.blade.php
+++ web/app/themes/econsult/resources/views/partials/head.blade.php
@@ -14,4 +14,14 @@
 	@endif
 	
 	@php wp_head() @endphp
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151309280-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-151309280-1');
+	</script>
+
 </head>
