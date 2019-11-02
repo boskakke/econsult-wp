55a5f553a5274ac385bb66e6e56a4cde3fa3f6bc
Bo Skakke <bo@skakke.dk>
2019-11-02 10:47:25 +0100

    headlines

--- web/app/themes/econsult/resources/views/partials/frontpage-cases.blade.php
+++ web/app/themes/econsult/resources/views/partials/frontpage-cases.blade.php
@@ -1,13 +1,15 @@
 <div class="section">		
-	<div class="section-header">
-		Cases
-	</div>
 	@php
 	$the_query = new WP_Query( array(
 		'post_type' => 'cases', 'posts_per_page' => 10
 	)); 
 	@endphp
 	@if ( $the_query->have_posts() )
+	
+		<div class="section-header">
+			Cases
+		</div>
+
 		<div class="teaser-flow">
 			@while ( $the_query->have_posts() ) 
 			@php
