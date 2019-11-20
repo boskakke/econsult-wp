7d9303bbf9944d7cbaaef824ee3cb10f7d0e850e
Bo Skakke <bo@skakke.dk>
2019-11-20 22:38:18 +0100

    ssvg and hero

--- web/app/themes/econsult/app/setup.php
+++ web/app/themes/econsult/app/setup.php
@@ -224,3 +224,11 @@ function wpdocs_custom_excerpt_length( $length ) {
     return 20;
 }
 add_filter( 'excerpt_length', __NAMESPACE__ . '\\wpdocs_custom_excerpt_length', 999 );
+
+
+// allow svg files to be uploaded
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', __NAMESPACE__ . '\\cc_mime_types');
