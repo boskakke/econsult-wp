89bebf5c9ecc3ad738af742a5215d36c14fc06b2
Bo Skakke <bo@skakke.dk>
2020-06-04 18:06:16 +0200

    footer

--- web/app/themes/econsult/resources/views/partials/contact.blade.php
+++ web/app/themes/econsult/resources/views/partials/contact.blade.php
@@ -1,9 +1,19 @@
+@php
+	$contact_header = get_field( 'contact_title', 'options' );
+	$contact_description = get_field( 'contact_summary', 'options' );
+@endphp
 <section class="section section--teasers mb-0">
 <div class="container">
+	@if ($contact_header)
 	<h2 class="section__header">
-		{!! the_field( 'contact_title', 'options' );!!}
+			{{ $contact_header }}
 	</h2>
-	<p class="section__summary">{!! the_field( 'contact_summary', 'options' );!!}</p>
+	@endif
+	@if ($contact_description)
+	<p class="section__description">
+		{!! $contact_description !!}
+	</p>
+	@endif
 
 	@php dynamic_sidebar('contact') @endphp
 </div>
