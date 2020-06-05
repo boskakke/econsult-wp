a48226bdce862dee33f75e8019f99e86f1ff3467
Bo Skakke <bo@skakke.dk>
2020-06-06 00:53:28 +0200

    footer

--- web/app/themes/econsult/app/setup.php
+++ web/app/themes/econsult/app/setup.php
@@ -16,7 +16,7 @@ use Roots\Sage\Template\BladeProvider;
  */
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_style('sage/googlefonts', 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600&display=swap', false, null);
+    wp_enqueue_style('sage/googlefonts', 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600,700&display=swap', false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }, 100);
 
