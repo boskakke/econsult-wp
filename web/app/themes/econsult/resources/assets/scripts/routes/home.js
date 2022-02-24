15d54183b30fa42cc7c9525bb0762de8d0271bb9
Bo Skakke <bo@skakke.dk>
2022-02-24 09:36:24 +0100

    Update home.js

--- web/app/themes/econsult/resources/assets/scripts/routes/home.js
+++ web/app/themes/econsult/resources/assets/scripts/routes/home.js
@@ -16,37 +16,37 @@ export default {
 			document.documentElement.style.setProperty('--vh-offset', vheight.vh + 'px');
 		})
 
+		const body = document.querySelector('body')
+		if (body.classList.contains('home')) {
+			const header = document.querySelector('.header');
+			const logo = header.querySelector('.header__col--logo');
+			const headerItems = header.querySelectorAll('.top-menu > .menu-item');
+			const pageTitle = document.querySelectorAll('.hero__title');
+			const images = document.querySelectorAll('.hero-fp__figure');
 
-		const header = document.querySelector('.header');
-    const logo = header.querySelector('.header__col--logo');
-    const headerItems = header.querySelectorAll('.top-menu > .menu-item');
-    const pageTitle = document.querySelectorAll('.hero__title');
-    const images = document.querySelectorAll('.hero-fp__figure');
-
-    const headerTL = gsap.timeline();
-    headerTL
-      .addLabel('start')
-      .fromTo(logo, { opacity: 0, y: -20 }, { opacity: 1, y: 0 })
-      .fromTo(
-        headerItems,
-        { opacity: 0, y: -20 },
-        { opacity: 1, y: 0, stagger: 0.1 },
-        'start+=.1'
-      )
-      .fromTo(
-        pageTitle,
-        { opacity: 0, y: 40 },
-        { opacity: 1, y: 0, duration: 2, ease: 'expo.out' },
-        '<.2'
-      )
-      .fromTo(
-        images,
-        { opacity: 0, y: 40 },
-        { opacity: 1, y: 0, duration: 2, ease: 'expo.out', stagger: 0.2 },
-        '<.2'
-      );
-
-		
+			const headerTL = gsap.timeline();
+			headerTL
+				.addLabel('start')
+				.fromTo(logo, { opacity: 0, y: -20 }, { opacity: 1, y: 0 })
+				.fromTo(
+					headerItems,
+					{ opacity: 0, y: -20 },
+					{ opacity: 1, y: 0, stagger: 0.1 },
+					'start+=.1'
+				)
+				.fromTo(
+					pageTitle,
+					{ opacity: 0, y: 40 },
+					{ opacity: 1, y: 0, duration: 2, ease: 'expo.out' },
+					'<.2'
+				)
+				.fromTo(
+					images,
+					{ opacity: 0, y: 40 },
+					{ opacity: 1, y: 0, duration: 2, ease: 'expo.out', stagger: 0.2 },
+					'<.2'
+				);
+		}
 
 
 
